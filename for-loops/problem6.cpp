88f5a10689a6ea1eb03ae507ffcf403884deb9d5
Abdul Wasio <wasioabdul820@gmail.com>
2025-10-20 20:37:46 -0700

    problem6.cpp

---
+++ for-loops/problem6.cpp
@@ -0,0 +1,14 @@
+//Print numbers in reverse order from N down to 1.
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int n;
+	cout<<"Enter Any Number: ";
+	cin>>n;
+	
+	for(int i = n; i>=1; i--){
+		cout<<i <<endl;
+
+	}
+}
