516453935af7b6142e2c1b0b5acf4b622b2b9625
Abdul Wasio <wasioabdul820@gmail.com>
2025-10-29 20:31:47 -0700

    Task 04.cpp

---
+++ labs/Lab 04/Task 04.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int a = 1;
+	int num;
+	cout<<"The Number For Table: ";
+	cin>>num;
+	
+	while(a<=10){
+		cout<<a*num <<endl;
+		a++;
+	}
+}
