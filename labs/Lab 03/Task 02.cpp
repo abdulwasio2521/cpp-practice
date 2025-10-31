16308615c1a70342d4f42ec38e4c5e948213cc7b
Abdul Wasio <wasioabdul820@gmail.com>
2025-10-31 05:14:42 -0700

    Task 02

---
+++ labs/Lab 03/Task 02.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float a;
+	float b = 3.14159;
+	
+	cout<<"The Radius Of Circle is: ";
+	cin>>a;
+	cout<<"The Area Is: " <<a*a*b;
+	return 0;
+}
