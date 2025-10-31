1775be7daf4e072c2c8dbde16890301c02996480
Abdul Wasio <wasioabdul820@gmail.com>
2025-10-31 05:20:59 -0700

    Task 07

---
+++ labs/Lab 03/Task 07.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	
+	cout<<"Enter Your Number: ";
+	cin>>a;
+	
+	cout << "\nInitial value: " << a << endl;
+    a += 5;  // add 5
+    cout << "After += 5 : " << a << endl;
+    a -=5;
+    cout << "After -= 5 : " << a << endl;
+    a *=5;
+    cout << "After -= 5 : " << a << endl;
+    a /=5;
+    cout << "After -= 5 : " << a << endl;
+}
