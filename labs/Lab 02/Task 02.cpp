3ea3306dc13fb91cd1fde9098fc50e7896475ad2
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-01 02:36:59 -0700

    Task 02

---
+++ labs/Lab 02/Task 02.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"The Size Of (int) is: "<<sizeof(int)<<" bytes"<<endl;
+	cout<<"The Size Of (float) is: "<<sizeof(float)<<" bytes"<<endl;
+	cout<<"The Size Of (short) is: "<<sizeof(short)<<" bytes"<<endl;
+	cout<<"The Size Of (double) is: "<<sizeof(double)<<" bytes"<<endl;
+	cout<<"The Size Of (long) is: "<<sizeof(long)<<" bytes"<<endl;
+	cout<<"The Size Of (char) is: "<<sizeof(char)<<" bytes"<<endl;
+	cout<<"The Size Of (bool) is: "<<sizeof(bool)<<" bytes"<<endl;
+}
