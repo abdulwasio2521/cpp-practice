4a7400d948a20520c95df31062a76ee7c3b130b8
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-01 02:38:01 -0700

    Addition

---
+++ labs/Lab 02/Task 03/Addition.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	int b;
+	cout<<"Enter First Number: ";
+	cin>>a;
+	cout<<"\nEnter Second Number: ";
+	cin>>b;
+	cout<<"\nThe Sum of Two Intergers is: "<<a+b<<endl;
+	return 0;
+}
