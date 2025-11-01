d37329cf64945b55d8e5d8c4b5573b12c4706c37
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-01 02:33:49 -0700

    Task 01

---
+++ labs/Lab 02/Task 01.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+string name;
+string rollno;
+cout<<"Enter Your Name: ";
+cin>>name;
+cout<<"Enter Your Roll No: ";
+cin>>rollno;
+cout<<"Welcome to Software Department: ";
+cout<<name;
+cout<<rollno;
+return 0;	
+}
