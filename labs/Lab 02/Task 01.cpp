29eedd74a4b542af96b26327b26afd32f44a706e
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-01 02:35:10 -0700

    Fix formatting and improve code structure

--- labs/Lab 02/Task 01.cpp
+++ labs/Lab 02/Task 01.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-string name;
-string rollno;
-cout<<"Enter Your Name: ";
-cin>>name;
-cout<<"Enter Your Roll No: ";
-cin>>rollno;
-cout<<"Welcome to Software Department: ";
-cout<<name;
-cout<<rollno;
+  string name;
+  string rollno;
+
+  cout<<"Enter Your Name: ";
+  cin>>name;
+  cout<<"Enter Your Roll No: ";
+  cin>>rollno;
+  cout<<"Welcome to Software Department: ";
+  
+  cout<<name;
+  cout<<rollno;
+  
 return 0;	
 }
