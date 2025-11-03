b78f55742e754084655414969155d3e9b49071f6
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-02 20:33:56 -0800

    Task 06

---
+++ labs/Lab 02/Task 06.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"Add Width: ";
+	cin>>a;
+	int b;
+	cout<<"Add Length: ";
+	cin>>b;
+	cout<<"The Area is: "<<a*b<<endl;
+	cout<<"The Perimeter is: "<<2*(a+b);
+}
