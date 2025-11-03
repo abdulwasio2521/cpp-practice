bd62d7ee9be4f8d168ba956710829ba55e90517c
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-02 20:29:12 -0800

    Multiplication

---
+++ labs/Lab 02/Task 03/Multiplication.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	int b;
+	cout<<"Enter First Number: ";
+	cin>>a;
+	cout<<"Enter Second Number: ";
+	cin>>b;
+	cout<<"\nThe Multiple Of Two Intergers: "<<a*b<<endl;
+	return 0;
+}
