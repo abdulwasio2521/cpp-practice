35ca00f17770f9d190a3f2f137d70be11f148e01
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-02 20:30:13 -0800

    Division

---
+++ labs/Lab 02/Task 03/Division.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+cout<<"Enter First Number: ";
+cin>>a;
+cout<<"Enter Second Number: ";
+cin>>b;
+cout<<"The Divison of Two Interger is: "<<a/b<<endl;
+return 0;
+}
