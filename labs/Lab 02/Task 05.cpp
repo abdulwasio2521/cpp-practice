7380d60fe6897d31e92d2dbc28665e2bf50995e2
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-02 20:33:13 -0800

    Task 05

---
+++ labs/Lab 02/Task 05.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a;
+	cout<<"Write an Integer: ";
+	cin>>a;
+	cout<<"The Square Is: " <<a*a<<endl;
+	cout<<"The Cube Is: "<<a*a*a<<endl;
+	cout<<"The Forth Power Is: "<<a*a*a*a<<endl;
+	cout<<"The Fifth Power Is: "<<a*a*a*a*a<<endl;
+	cout<<"The Square Root Is: "<<endl;
+}
