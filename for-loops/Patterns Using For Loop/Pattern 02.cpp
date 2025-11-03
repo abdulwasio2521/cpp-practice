19963437791e704435c9f5687d88f8ed82a5e0a8
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-03 09:34:16 -0800

    Pattern 02
    This program prints a square pattern of asterisks based on user input.

---
+++ for-loops/Patterns Using For Loop/Pattern 02.cpp
@@ -0,0 +1,20 @@
+//Print the following Pattern,  when n = 4
+//* * * *
+//* * * *
+//* * * *
+//* * * *
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int n;
+	cout<<"Enter Any Number: ";
+	cin>>n;
+	
+	for(int i = 0; i<n; i++){
+		for(int j = 0; j<n; j++){
+			cout<<"* ";
+		}
+	cout<<endl;
+	}
+}
