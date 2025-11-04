d5ee38d150bed3bf89d7614ee49528b249b925ff
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-04 08:22:45 -0800

    Refactor Division.cpp for cleaner variable declaration

--- labs/Lab 02/Task 03/Division.cpp
+++ labs/Lab 02/Task 03/Division.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-int a;
-int b;
-cout<<"Enter First Number: ";
-cin>>a;
-cout<<"Enter Second Number: ";
-cin>>b;
-cout<<"The Divison of Two Interger is: "<<a/b<<endl;
-return 0;
+  int a, b;
+  
+  cout<<"Enter First Number: ";
+  cin>>a;
+  cout<<"Enter Second Number: ";
+  cin>>b;
+  
+  cout<<"The Divison of Two Interger is: "<<a/b<<endl;
+  return 0;
+  s
 }
