3eaa87a795eac643d587eefdc719e5594b7f601b
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-04 08:02:01 -0800

    Pattern 11

---
+++ for-loops/Patterns Using For Loop/Pattern 11.cpp
@@ -0,0 +1,24 @@
+//Print the following Pattern, when n = 4
+// A
+// B C
+// D E F
+// G H I J
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int n;
+	cout<<"Enter Any Number: ";
+	cin>>n;
+	
+	char ch = 'A';
+	
+	for(int i = 1; i<=n; i++){
+		for(int j = 1; j<=i; j++){
+			cout<<ch <<" ";
+			ch = ch + 1;
+		}
+	cout<<endl;
+	}
+	
+}
