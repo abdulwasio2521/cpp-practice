688e8b79fb198a7312c60a64f8dcd093e3938ba5
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-04 08:00:13 -0800

    Pattern 08

---
+++ for-loops/Patterns Using For Loop/Pattern 08.cpp
@@ -0,0 +1,21 @@
+//Print the following Pattern, when n = 4
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int n;
+	cout<<"Enter Any Number: ";
+	cin>>n;
+	
+	for(int i = 1; i<=n; i++){
+		for(int j = 1; j<=i; j++){
+			cout<<j <<" ";
+		}
+	cout<<endl;
+	}
+	
+}
