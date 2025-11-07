5df25ac42ee7b2084f4d2d164514016dda322750
Abdul Wasio <wasioabdul820@gmail.com>
2025-11-07 04:14:41 -0800

    Update Task 04.cpp

--- labs/Lab 03/Task 04.cpp
+++ labs/Lab 03/Task 04.cpp
@@ -3,8 +3,9 @@ using namespace std;
 int main(){
 	int a;
 	
-	cout<<"Enter Your Number: ";
+	cout<<"Enter Any Number: ";
 	cin>>a;
+	
 	(a % 2 == 0) ? cout << a << " is Even" << endl //a % 2 == 0 checks if number is divisble by 2
 	: cout << a << " is Odd" << endl;
     return 0;
